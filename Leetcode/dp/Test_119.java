7b6651ba9292cf07f86dd2f9dc60467b71a45707
luzhi <luzhi.jzh@gmail.com>
2021-04-17 15:40:37 +0800

    11

--- Leetcode/dp/Test_119.java
+++ Leetcode/dp/Test_119.java
@@ -4,6 +4,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Test_119 {
+    /*
+    在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+    示例:
+
+    输入: 3
+    输出: [1,3,3,1]
+     */
     public static void main(String[] args) {
         List<Integer> result = getRow(0);
         System.out.println(result);
